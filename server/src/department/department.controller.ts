0a87cc56153f54f7e177df4ab75252f5fac4e0dc
24bartixx <jestgitbartix@gmail.com>
2026-02-02 16:16:13 +0100

    fix: remove unused tests

--- server/src/department/department.controller.ts
+++ server/src/department/department.controller.ts
@@ -6,33 +6,33 @@ import { UpdateDepartmentDto } from "./dto/update-department.dto";
 
 @Controller('departments')
 export class DepartmentController {
-    constructor(private readonly departmentService: DepartmentService) {}
+  constructor(private readonly departmentService: DepartmentService) {}
 
-    @Post()
-    async create(@Body() createDepartmentDto: CreateDepartmentDto): Promise<Department> {
-        return await this.departmentService.create(createDepartmentDto);
-    }
+  @Post()
+  async create(@Body() createDepartmentDto: CreateDepartmentDto): Promise<Department> {
+    return await this.departmentService.create(createDepartmentDto);
+  }
 
-    @Get()
-    async findAll(): Promise<Department[]> {
-        return await this.departmentService.findAll();
-    }
+  @Get()
+  async findAll(): Promise<Department[]> {
+    return await this.departmentService.findAll();
+  }
 
-    @Get(':id')
-    async findOne(@Param('id', ParseIntPipe) id: number): Promise<Department> {
-        return await this.departmentService.findOne(id);
-    }
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Department> {
+    return await this.departmentService.findOne(id);
+  }
 
-    @Patch(':id')
-    async update(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() updateDepartmentDto: UpdateDepartmentDto,
-    ): Promise<Department> {
-        return await this.departmentService.update(id, updateDepartmentDto);
-    }
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateDepartmentDto: UpdateDepartmentDto,
+  ): Promise<Department> {
+    return await this.departmentService.update(id, updateDepartmentDto);
+  }
 
-    @Delete(':id')
-    async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return await this.departmentService.remove(id);
-    }
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return await this.departmentService.remove(id);
+  }
 }
